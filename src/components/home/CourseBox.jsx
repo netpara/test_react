e412b4cad47308ca964304bd163705745c9898d0
parastoo <parastookachkoly@gmail.com>
2025-11-22 11:40:47 +0330

    new part lern git push

--- src/components/home/CourseBox.jsx
+++ src/components/home/CourseBox.jsx
@@ -1,8 +1,8 @@
 export default function CourseBox(props){
 return(
 <div>
-    <h2>{props?.title}</h2>
-    <p>{props?.details}</p>
+    <h2>{props?.course?.title}</h2>
+    <p>{props?.course?.details}</p>
     <span>time : 10</span>
 </div>
 )
--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -6,9 +6,12 @@ return(
     <h1>
         hi parstoo
     </h1>
-    <CourseBox title="course number one" detailes="this is about the course" />
-    <CourseBox title="javascript course" detailes="javascript details..." />
-    <CourseBox title="react course " detailes="react course details ..." />
+    <CourseBox course={{
+        title: "course number one",
+        details : "this is about the course"
+    }}/>
+    {/* <CourseBox title="javascript course" detailes="javascript details..." />
+    <CourseBox title="react course " detailes="react course details ..." /> */}
 </div>
 )
 }
